46e98b8014238f69210dbf366f0517c1b55330e1
rahul9890 <rkumbhar911@gmail.com>
2025-03-27 21:45:13 +0530

    added manage user component

--- src/Dashboard/ManageUsers.tsx
+++ src/Dashboard/ManageUsers.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface User {
+  userId: number;
+  userName: string;
+  userEmail: string;
+  userPassword: string;
+}
 
 const ManageUsers: React.FC = () => {
-  return <div>ManageUsers</div>;
+  const [userList, setUserList] = useState<User[]>();
+  const baseUserURL = "http://localhost:8080/users";
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const responseData = await axios.get(baseUserURL);
+        if (responseData.status === 200) {
+          let data = responseData.data;
+          setUserList(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="m-4">
+      <h2>Users</h2>
+      <table className="table table-info table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList && userList.map((item) => (
+            <tr key={item.userId}>
+              <td>{item.userName}</td>
+              <td>{item.userEmail}</td>
+              <td>
+                <span>
+                  <button className="btn btn-primary m-1">Edit</button>
+                </span>
+                <span>
+                  <button className="btn btn-primary">Delete</button>
+                </span>
+              </td>
+            </tr>
+          ))}
+         </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ManageUsers;
