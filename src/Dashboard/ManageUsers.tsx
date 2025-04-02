f9caf1038fd60c6c075241228f10b7394e2c4fb9
rahul9890 <rkumbhar911@gmail.com>
2025-04-02 22:42:12 +0530

    added changes for delete modal

--- src/Dashboard/ManageUsers.tsx
+++ src/Dashboard/ManageUsers.tsx
@@ -12,7 +12,10 @@ interface User {
 const ManageUsers: React.FC = () => {
   const [userList, setUserList] = useState<User[]>();
   const baseUserURL = "http://localhost:8080/users";
-
+ const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -29,37 +32,84 @@ const ManageUsers: React.FC = () => {
   }, []);
 
 
-  const navigate = useNavigate();
-
+ 
   return (
-    <div className="m-4">
-      <h2>Users</h2>
-      <table className="table table-info table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {userList && userList.map((item) => (
-            <tr key={item.userId}>
-              <td>{item.userName}</td>
-              <td>{item.userEmail}</td>
-              <td>
-                <span>
-                  <button className="btn btn-primary m-1" onClick={()=>navigate("/edituser",{state:{user:item}})}>Edit</button>
-                </span>
-                <span>
-                  <button className="btn btn-primary">Delete</button>
-                </span>
-              </td>
+    <>
+      <div className="m-4">
+        <h2>Users</h2>
+        <table className="table table-info table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th></th>
             </tr>
-          ))}
-         </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {userList &&
+              userList.map((item) => (
+                <tr key={item.userId}>
+                  <td>{item.userName}</td>
+                  <td>{item.userEmail}</td>
+                  <td>
+                    <span>
+                      <button
+                        className="btn btn-primary m-1"
+                        onClick={() =>
+                          navigate("/edituser", { state: { user: item } })
+                        }
+                      >
+                        Edit
+                      </button>
+                    </span>
+                    <span>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => setShowModal(true)}
+                      >
+                        Delete
+                      </button>
+                    </span>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        {showModal && (
+          <div className="modal fade show d-block">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Confirm Delete</h5>
+                  <button
+                    type="button"
+                    className="btn-close"
+                    onClick={() => setShowModal(false)}
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <p>Are you sure you want to delete</p>
+                </div>
+                <div className="modal-footer d-flex justify-content-between">
+                  <button type="button" className="btn btn-danger">
+                    Confirm
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
