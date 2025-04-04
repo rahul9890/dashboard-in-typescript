0a55d15c40a7081994e069fde25e67e446edface
rahul9890 <rkumbhar911@gmail.com>
2025-04-04 22:11:23 +0530

    added table in manage documents

--- src/Dashboard/ManageDocument.tsx
+++ src/Dashboard/ManageDocument.tsx
@@ -26,7 +26,35 @@ const ManageDocument: React.FC = () => {
     fetchData();
   }, []);
 
-  return <div>ManageDocument</div>;
+  return (
+    <div>
+      <h2>My Uploads</h2>
+      <table className="table table-info table-bordered">
+        <thead>
+          <tr>
+            <th>File Name</th>
+            <th>File Type</th>
+            <th>File Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userDocList &&
+            userDocList.length > 0 &&
+            userDocList.map((item) => (
+              <tr key={item.documentId}>
+                <td>{item.fileName}</td>
+                <td>{item.fileType}</td>
+                <td>
+                  <button>Edit</button>
+                  <button>Download</button>
+                  <button>Delete</button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ManageDocument;
