09bbccf10b9b299bb411c46a0e97a8ed73aa8e57
rahul9890 <rkumbhar911@gmail.com>
2025-04-09 22:07:53 +0530

    added some chages

--- src/UserOperations/CreateUserGoals.tsx
+++ src/UserOperations/CreateUserGoals.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-
- 
-
-const CreateUserGoals: React.FC = ()=>{
-    return (<div>in create goals</div>)
+interface CreateGoals {
+  goalTitle: string;
+  goalType: string;
+  goalDescription: string;
+  goalPriority: string;
+  dueDate: Date;
 }
+
+const CreateUserGoals: React.FC = () => {
+   
+
+
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card shadow-lg p-4" style={{ width: "50rem" }}>
+        <div className="card-body text-center">
+          <h2>Create Goal</h2>
+          <form>
+          
+           
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CreateUserGoals;
